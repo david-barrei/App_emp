3191db85393873bcfa78e2d8417d4e68bd0db4a5
david-barrei <davbarreiro4@gmail.com>
2023-10-22 23:38:49 -0500

    AGREGANDO CONFIGURARIONES

--- empleado/applications/departamento/apps.py
+++ empleado/applications/departamento/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class DepartamentoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'applications.departamento'
+    default_auto_field = 'django.db.models.BigAutoField' 
+    name = 'applications.departamento' #agregando la ruta departamento
+    
--- empleado/applications/empleados/apps.py
+++ empleado/applications/empleados/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class EmpleadosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'applications.empleados'
+    name = 'applications.empleados' # agregando la ruta de empleados
